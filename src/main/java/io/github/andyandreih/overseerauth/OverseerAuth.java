9542e459fd8f547eb0e65a4a443ca1465d06d271
Andrei I. Hava <hava.ionut@yahoo.com>
2015-02-02 18:18:30 +0200

    Added onEnable and onDisable event handlers

--- src/main/java/io/github/andyandreih/overseerauth/OverseerAuth.java
+++ src/main/java/io/github/andyandreih/overseerauth/OverseerAuth.java
@@ -4,5 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OverseerAuth extends JavaPlugin
 {
+    @Override
+    public void onEnable()
+    {
+        getLogger().info("onEnable has been invoked!");
+    }
 
+    @Override
+    public void onDisable()
+    {
+        getLogger().info("onDisable has been invoked!");
+    }
 }
